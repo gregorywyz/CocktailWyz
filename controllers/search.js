ef68f193086e269585ab8cedb6ad6bb401c0b80b
gregorywyz <gregwyszynski@gmail.com>
2015-04-14 08:44:53 -0700

    API is running, search result and show page are displaying data

---
+++ controllers/search.js
@@ -0,0 +1,174 @@
+// ~~~ load node modules  ~~~
+// var db = require("../models");  // require databases
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+
+// render search results page with only categories of 'drinks'
+router.get('/name', function(req,res) {
+
+  var drinkList = [];
+  var query = req.query.q;
+  // route for search of recipes
+  var bigOvenUrl = 'http://api.bigoven.com/recipes';
+
+  // set up query for url
+  var queryData = {
+    pg:1,
+    rpp:20,
+    api_key:process.env.BO_KEY,
+    title_kw:query
+  };
+
+  // pass url in as object, change headers to return JSON
+  request({
+    url:bigOvenUrl,
+    qs:queryData,
+    headers:{
+      'Accept':'application/json'
+    }
+  },function(error,response,data) {
+
+    if (!error && response.statusCode == 200) {
+      var searchResults = JSON.parse(data);
+      // console.log('searchRsults is:',typeof searchResults);
+      searchResults.Results.forEach(function(drink,idx) {
+        if (drink.Category === 'Drinks') {
+          drinkList.push(drink);
+          // console.log('name:',drink.Title);
+          // console.log('recipeID:',drink.RecipeID);
+        };
+      });
+      // console.log(searchResults.Results[0].Title);
+      console.log(drinkList.length)
+      // res.send(searchResults);
+      res.render('drinks/results',{Results:drinkList});
+
+    } else {
+      res.send('Sorry no cocktails here, try another drink');
+    };
+  });
+});
+
+
+// render show page with recipe details
+router.get('/name/:id', function(req,res) {
+
+  var recID = req.params.id;
+  // route for details on specific recipe
+  var bigOvenUrl2 = 'http://api.bigoven.com/recipe/' + recID + '?api_key=' + process.env.BO_KEY;
+
+  request({
+    url:bigOvenUrl2,
+    headers:{
+      'Accept':'application/json'
+    }
+  },function(error,response,data) {
+    if (!error && response.statusCode == 200) {
+      var recipe = JSON.parse(data);
+      // res.send(recipe);
+      res.render('drinks/show',recipe);
+    };
+  });
+});
+
+
+
+module.exports = router;
+
+
+
+
+// // ~~~ load node modules  ~~~
+// // var db = require("../models");  // require databases
+// var express = require('express');
+// var request = require('request');
+// var router = express.Router();
+
+// router.get('/name', function(req,res) {
+
+//   var query = req.query.q;
+//   var url = 'http://addb.absolutdrinks.com/quickSearch/drinks/' + query + '/?Apikey=' + process.env.ADDB_KEY;
+//   console.log('query',query)
+//   console.log('url',url);
+
+//   request(url, function(error,response,data) {
+//     var locals = JSON.parse(data);
+
+//     if (locals.result) {
+//       console.log('GET search:',locals) // array of drinks
+//       res.render('drinks/results',locals);
+//     } else {
+//       res.send('please pick another cocktail');
+//     };
+
+//     // res.render('drinks/results');
+//   })
+// });
+
+// router.get('/name/:id', function(req,res) {
+
+//   var query = req.params.id;
+//   var url = 'http://addb.absolutdrinks.com/drinks/' + query + '/?Apikey=' + process.env.ADDB_KEY;
+//   console.log('query',query)
+//   console.log('url',url);
+
+//   request(url, function(error,response,data) {
+//     var locals = JSON.parse(data);
+//     if (locals.result) {
+//       // res.send(locals.result);
+//       res.render('drinks/show',locals);
+//     } else {
+//       res.send('sorry we do not have any info on that cocktail');
+//     }
+//   })
+// });
+
+// router.get('/ingredients', function(req,res) {
+
+//   var query = req.query.i;
+//   var urlName = 'http://addb.absolutdrinks.com/quickSearch/ingredients/' + query + '/?Apikey=' + process.env.ADDB_KEY;
+
+
+//   var ingredientOfChoice = '';
+
+//   // search for list of ingredients
+//   request(urlName, function(error,response,data) {
+
+//     var locals = JSON.parse(data);
+
+//     locals.result.forEach(function(ingredient, idx) {
+//       if (ingredient.name.toLowerCase() == query.toLowerCase()) {
+//         console.log('~~~~~ We have a match ~~~~',ingredient.id)
+//         ingredientOfChoice = ingredient.id;
+//       }
+//       console.log('ingredient' + idx + ':',ingredient.id);
+//     })
+
+//     console.log('ingredientOfChoice', ingredientOfChoice);
+
+
+//   //!!!! need to send in multiple ingredients as /with/ingredient_id...
+//   // can possibly build up an array of ingredients, then itterate through to set as url
+
+//    // set url for drink w/ ingredient search and search
+//     var urlId = 'http://addb.absolutdrinks.com/drinks/with/' + ingredientOfChoice + '/?Apikey=' + process.env.ADDB_KEY;
+//     request(urlId, function(error,response,data) {
+//       var locals = JSON.parse(data);
+//       // res.send(data);
+
+//       res.render('drinks/results',locals);
+//     });
+
+
+//     // res.render('drinks/results');
+//   })
+
+
+
+// });
+
+
+
+// module.exports = router;
---
+++ index.js
@@ -0,0 +1,127 @@
+// ~~~ load node modules  ~~~
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var searchCtrl = require('./controllers/search.js')
+var app = express();
+
+
+// ~~~ setup express ~~~
+app.set("view engine", "ejs");  // tell express to use .ejs files
+app.use(express.static(__dirname + "/public"));  // tell express where static assets are to be served
+app.use(bodyParser.urlencoded({extended: false}));  // use body-parser
+app.use('/search', searchCtrl);
+
+// render home page
+app.get('/', function(req,res) {
+  res.render('index');
+  // res.send('hello WORLD!!!!')
+});
+
+app.get('/search/byName',function(req,res) {
+  res.render('search/byName');
+});
+
+// app.get("/", function(req,res) {
+
+
+//   var drinkList = [];
+//   var query = 'mojito';
+//   var bigOvenUrl = 'http://api.bigoven.com/recipes';
+
+
+//   // set up query for url
+//   var queryData = {
+//     pg:1,
+//     rpp:20,
+//     api_key:'dvx59qENtcYtAr9f0238g7auJai7GeUx',
+//     title_kw:query
+//   };
+
+//   // pass url in as object, change headers to return JSON
+//   request({
+//     url:bigOvenUrl,
+//     qs:queryData,
+//     headers:{
+//       'Accept':'application/json'
+//     }
+//   },function(error,response,data) {
+
+//     if (!error && response.statusCode == 200) {
+//       var searchResults = JSON.parse(data);
+//       searchResults.Results.forEach(function(drink,idx) {
+//         if (drink.Category === 'Drinks') {
+//          console.log('name:',drink.Title);
+//          console.log('recipeID:',drink.RecipeID);
+//          drinkList.push(drink.RecipeID);
+//         }
+
+//       })
+//       // console.log(searchResults.Results[0].Title);
+
+//       console.log('drinkList:',drinkList);
+//       res.send(searchResults);
+
+
+//     };
+//   })
+
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+// set server to listen for
+app.listen(3000, function() {
+  console.log("~~~~~~~~~~ Server started on port 3000 ~~~~~~~~~~");
+});
+
+// MOVED DOWN MONDAY - refactor for new API
+// // var query = 'screwdriver';
+
+// // request('http://addb.absolutdrinks.com/drinks/' + query + '/?Apikey=5fbe1c268530417d9f2a253da2a6ba5d', function(error,response,data) {
+// //   if (!error && response.statusCode == 200) {
+// //     var locals = JSON.parse(data)
+// //     console.log('screwdriver',locals);
+// //   }
+// // })
+
+// // render home page
+// app.get('/', function(req,res) {
+//   res.render('index');
+// });
+
+// app.get('/search/byName',function(req,res) {
+//   res.render('search/byName');
+// });
+
+// app.get('/search/byIngredient',function(req,res) {
+//   res.render('search/byIngredient');
+// });
+
+// // app.get('/ingredients', function(req,res) {
+// //   res.send(req.params.i);
+// // })
+
+
+// request('http://addb.absolutdrinks.com/drinks/with/absinthe/with/lemon/with/cognac/?Apikey=' + process.env.ADDB_KEY, function(error,response,data) {
+//   if (!error && response.statusCode == 200) {
+//     var locals = JSON.parse(data)
+//     console.log('~~~~~name~~~~~',locals.result[0].name);
+//     console.log('~~~~~numOfIngredients~~~~~',locals.result[0].ingredients.length)
+//     for (var i = 0; i < locals.result[0].ingredients.length; i++) {
+//       console.log('~~~~~ingredient ' + i + '~~~~~', locals.result[0].ingredients[i].id)
+//     }
+//     console.log('~~~~~description~~~~~',locals.result[0].description);
+//     console.log('~~~~~descriptionPlain~~~~~',locals.result[0].descriptionPlain);
+//     console.log('~~~~~story~~~~~',locals.result[0].story);
+//   }
+// })
