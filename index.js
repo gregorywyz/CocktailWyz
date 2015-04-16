cf277292c6187806818f6bb67beff9378a83eaaf
gregorywyz <gregwyszynski@gmail.com>
2015-04-16 10:33:24 -0700

    adding procfile

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/', function(req,res) {
 
 
 // set server to listen for
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("~~~~~~~~~~ Server started on port 3000 ~~~~~~~~~~");
 });
 
