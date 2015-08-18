7ac636d22c482102c261578d3a98b7455af06cb0
gregorywyz <gregwyszynski@gmail.com>
2015-08-17 22:00:38 -0700

    commented and added validation to user model relations

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,7 +21,7 @@ router.post('/login',function(req,res) {
           if (err) throw err;
 
           if (result) {
-            //store user to session
+            // store user to session
             req.session.user = {
               id: user.id,
               email: user.email,
@@ -30,12 +30,12 @@ router.post('/login',function(req,res) {
             req.flash('success','You have been logged in.');
             res.redirect('/');
           } else {
-            req.flash('danger','Invalid password.');
+            req.flash('danger','Oops! That didn\'t work. Please check your email and password and try again.');
             res.redirect('/auth/login');
           };
         });
       } else {
-        req.flash('danger','Unknown user. Please sign up.');
+        req.flash('danger','We do not have any records of you. Please sign up and become a new member!');
         res.redirect('/auth/signup');
       };
     });
@@ -51,24 +51,37 @@ router.get('/signup',function(req,res) {
 
 // CREATE - add new user to db
 router.post('/signup', function(req,res) {
+  // gather user data
   var userQuery = {email: req.body.email};
   var userData = {
     email: req.body.email,
     name: req.body.name,
     password: req.body.password
   };
-
+  // add user to database and log in on success
   db.user.findOrCreate({where: userQuery, defaults: userData})
     .spread(function(user,created) {
       if (created) {
-        req.flash('success','New user created. Please login.');
-        res.redirect('/auth/login');
-      } else {
-        req.flash('danger','e-mail already exists.');
+        req.flash('success','Thank you for signing up, ' + user.name + '! Welcome and drink responsibly.');
+            // store user to session
+            req.session.user = {
+              id: user.id,
+              email: user.email,
+              name: user.name
+            };
+        res.redirect('/');
+      // flash message if user already exists
+      } else if (user) {
+        req.flash('danger','Sorry, an account already exists with this email. Please choose another or sign up.');
         res.redirect('/auth/signup');
-      };
+      } else {
+         req.flash('danger','Oops! Something went wrong. Please try again.');
+        res.redirect('/auth/signup');
+      }
+    // check for backend validations
     }).catch(function(error) {
       if (error) {
+        // construct array for displaying multiple errors
         if(Array.isArray(error.errors)) {
           error.errors.forEach(function(errorItem) {
             req.flash('danger',errorItem.message);
@@ -87,6 +100,7 @@ router.post('/signup', function(req,res) {
 
 
 // GET - logs out user
+// deletes user session
 router.get('/logout',function(req,res){
     delete req.session.user;
     req.flash('info','You have been logged out.')
--- models/user.js
+++ models/user.js
@@ -3,28 +3,40 @@
 var bcrypt = require('bcrypt');
 
 module.exports = function(sequelize, DataTypes) {
+  // define user model and validations before create
   var user = sequelize.define("user", {
-    name: DataTypes.TEXT,
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'You must not leave the name field empty.'
+        }
+      }
+    },
     email: {
       type: DataTypes.TEXT,
+      allowNull: false,
       validate: {
-        isEmail: true
-      },
-      allowNull: false
+        isEmail: {
+          msg: 'You must provide a valid email.'
+        }
+      }
     },
     password: {
       type: DataTypes.TEXT,
+      allowNull: false,
       validate: {
         len: {
           args: [4,20],
           msg: 'Password must be between 4 and 20 characters long.'
         }
-      },
-      allowNull: false
+      }
     }
   }, {
     hooks: {
       beforeCreate: function(user,options,sendback) {
+      // use bcrypt to secure password
         bcrypt.hash(user.password, 10, function(err,hash) {
           if (err) { throw err; };
           user.password = hash;
@@ -34,7 +46,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        // associations are defined here
         models.user.hasMany(models.favorite);
         models.user.hasMany(models.shopping);
       }
